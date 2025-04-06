69e5e0858bdf01511c1a8ad07971be03132a7515
vvvvElya <elyav8723@gmail.com>
2025-04-06 20:57:46 +0200

    upravené environmentálne premenne pre bezpečné nasadenie

--- exchange_rates/settings.py
+++ exchange_rates/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['exchange-rates-app.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
